ea2f932f561223fb37b8d88c8e131ce078848ae8
FullTimeScam <418.gingin@gmail.com>
2024-05-16 15:43:53 +0900

    로딩 만들기

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,10 +6,13 @@ import ChatlistCard from "../components/ChatlistCard";
 const Home = () => {
   const [content, setContent] = useState("");
   const [chatlist, setChatlist] = useState([]);
+  const [isLoading, setLoading] = useState();
 
   const onSubmitChat = async (e) => {
     try {
       e.preventDefault();
+
+      setLoading(true); // 로딩 시작
 
       if (!content) return;
 
@@ -54,9 +57,13 @@ const Home = () => {
 
       localStorage.setItem("savedChatlist", JSON.stringify(savedChatlist));
 
+      setLoading(false); // 로딩이 끝났다
+
       setChatlist([newChat, ...chatlist]);
     } catch (error) {
       console.error(error);
+
+      setLoading(true); //에러가 떴을 때 다시 로딩중으로 돌아가기
     }
   };
 
@@ -64,37 +71,37 @@ const Home = () => {
     console.log(chatlist);
   }, [chatlist]);
 
-  const onClickSave = () => {
-    localStorage.setItem("test", "안녕하세요");
-  };
-
   return (
     <div className="mt-8 flex flex-col justify-center">
       <form
         className="flex justify-center items-center"
         onSubmit={onSubmitChat}
       >
-        <input
-          className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-orange-200 focus:border-orange-400"
-          type="text"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
+        {isLoading ? (
+          <div>로딩중</div>
+        ) : (
+          <>
+            <input
+              className="text-2xl p-2 focus:outline-none rounded-lg border-2 border-orange-200 focus:border-orange-400"
+              type="text"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            />
 
-        <button
-          className="ml-4 flex justify-center items-center bg-orange-200 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-orange-300 hover:bg-orange-300"
-          type="submit"
-        >
-          <TbRobot /> 검색
-        </button>
+            <button
+              className="ml-4 flex justify-center items-center bg-orange-200 text-2xl px-4 py-[10px] rounded-full shadow-md shadow-orange-300 hover:bg-orange-300"
+              type="submit"
+            >
+              <TbRobot /> 검색
+            </button>
+          </>
+        )}
       </form>
       <ul>
         {chatlist.map((v, i) => (
           <ChatlistCard key={i} question={v.question} answer={v.answer} />
         ))}
       </ul>
-
-      <button onClick={onClickSave}>저장</button>
     </div>
   );
 };
