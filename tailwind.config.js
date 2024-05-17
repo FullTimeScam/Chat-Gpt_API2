da099f656b15865631c8078b836ce734c9bbaf7b
FullTimeScam <418.gingin@gmail.com>
2024-05-17 09:30:17 +0900

    도스색상 등록 완료

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        DOSBlue: "rgb(0, 0, 170)",
+        DOSPurple: "rgb(128, 0, 128)",
+      },
+    },
   },
   plugins: [],
 };
